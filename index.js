8379cdc7274be2c3afcc08cbaf613b8ccc697555
Nwsheats <nwsheats@gmail.com>
2022-11-03 18:30:13 -0500

    changed class code

---
+++ index.js
@@ -0,0 +1,6 @@
+// different functions (array) for each role, because they have different questions
+// different inquirer prompt for each one
+
+
+// function for adding each class, building the team
+// need to call functions again later in code, so it has a loop to it
