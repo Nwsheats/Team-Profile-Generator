6adb4fcb27ced671620c0d915a7a8709a0bae921
Nwsheats <nwsheats@gmail.com>
2022-11-08 21:40:19 -0600

    fixed jest tests

--- __tests__/manager.test.js
+++ __tests__/manager.test.js
@@ -2,10 +2,8 @@ const Manager = require('../lib/manager');
 
 describe("getRole method", () => {
         it("returns a string of a role", () => {
-            const testManager = new Manager([
-                { name: "Heather", id: 2, email: "heather@email.com", officeNumber: 44 }
-              ]);
-              testManager.getRole(Manager);
-              expect(testManager).toReturn('Manager');
+            const testManager = new Manager("Heather", 2, "heather@email.com", 44);
+            const getRoleTest = testManager.getRole();
+              expect(getRoleTest).toBe('Manager');
             });
         });
